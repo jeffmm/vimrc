5c3060c8f9501e1450a92b3df0b214a614d1f8a5
Jeffrey Moore <jeffrey.m.moore@colorado.edu>
2019-09-20 18:02:33 -0600

    Updated vimrc install script

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
-vim +PluginInstall +qall
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    if hash git 2>/dev/null; then
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    else
+        echo "Git not found on this system! Install git and run: "
+        echo "  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
+        exit 1
+    fi
+fi
+if hash vim 2>/dev/null; then
+    vim +PluginInstall +qall
+else
+    echo "Vim not found on this system! Install vim and run: "
+    echo "  vim +PluginInstall +qall"
+fi
+
 
